5d7c6b3af8961c1328859f987982391fc7159c38
tran0370 <tran0370@gmail.com>
2019-06-04 11:08:31 -0700

    Completed Lab01, Stick Figure

---
+++ Code/Scott/lab01.py
@@ -0,0 +1,31 @@
+"""
+Name:           Scott Tran
+Date            6/4/2019
+Assignment:     Make a stick figure using Turtle Module
+"""
+
+import turtle
+ 
+t = turtle.Turtle()
+
+t.circle(50)
+t.right(90)
+t.forward(20)
+t.left(90)
+t.forward(75)
+t.right(180)
+t.forward(150)
+t.left(180)
+t.forward(75)
+t.right(90)
+t.forward(100)
+t.left(30)
+t.forward(100)
+t.backward(100)
+t.right(60)
+t.forward(100)
+t.backward(100)
+t.left(30)
+t.forward(20)
+
+input()
