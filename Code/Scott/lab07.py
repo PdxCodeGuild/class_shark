3ac1fe75eaab947853ae991891cad60c95257342
tran0370 <tran0370@gmail.com>
2019-06-06 12:18:08 -0700

    completed lab07.py

--- Code/Scott/lab07.py
+++ Code/Scott/lab07.py
@@ -10,25 +10,33 @@ import random
 
 pick = ['rock', 'paper', 'scissors']
 
-user_pick = input("Lets play paper-rock-scissors! Choose one: ")
+Play = True
 
-computer_pick = random.choice(pick)
+while Play:
 
-print(f"The compuer selects {computer_pick}.")
+    user_pick = input("Lets play paper-rock-scissors! Choose one: ")
 
-if user_pick == computer_pick:
-    print("Tie.")
-elif user_pick == 'rock' and computer_pick == 'scissors':
-    print("You win!")
-elif user_pick == 'rock' and computer_pick == 'paper':
-    print("You lose!")
-elif user_pick == 'scissors' and computer_pick == 'rock':
-    print("You lose!")
-elif user_pick == 'scissors' and computer_pick == 'paper':
-    print("You win!")
-elif user_pick == 'paper' and computer_pick == 'scissors':
-    print("You lose!")
-elif user_pick == 'paper' and computer_pick == 'rock':
-    print("You win!")
-else:
-    print(f"Try again, {user_pick}")
+    computer_pick = random.choice(pick)
+
+    print(f"The computer selects {computer_pick}.")
+
+    if user_pick == computer_pick:
+        print("Tie.")
+    elif user_pick == 'rock' and computer_pick == 'scissors':
+        print("You win!")
+    elif user_pick == 'rock' and computer_pick == 'paper':
+        print("You lose!")
+    elif user_pick == 'scissors' and computer_pick == 'rock':
+        print("You lose!")
+    elif user_pick == 'scissors' and computer_pick == 'paper':
+        print("You win!")
+    elif user_pick == 'paper' and computer_pick == 'scissors':
+        print("You lose!")
+    elif user_pick == 'paper' and computer_pick == 'rock':
+        print("You win!")
+    else:
+        print(f"That wasn't one of the choices....")
+
+    again = input("\nWould you like to ask again? please type 'yes' or 'no':  ")
+    if again == "no":
+        Play = False
