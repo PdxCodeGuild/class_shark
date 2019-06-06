db9a03604d4e37ad122a1f6265497568fe76aeb9
tran0370 <tran0370@gmail.com>
2019-06-06 15:13:56 -0700

    Fixed lab08.py

--- Code/Scott/lab08.py
+++ Code/Scott/lab08.py
@@ -6,14 +6,11 @@ Assignment:     Convert a dollar amount into a number of coins
 
 coin = int(input("What is the dollar amount?"))
 
-print("\nIn your purse:")
+print("\nIn your purse:", end="")
 
 quarters = int(coin//25)
 dimes = int((coin - (quarters*25))//10)
 nickels = int((coin - (quarters*25)-(dimes*10))//5)
 pennies = int((coin - (quarters*25)-(dimes*10)-(nickels*5)))
 
-print(f"there are {quarters} quarter(s),")
-print(f"there are {dimes} dime(s),")
-print(f"there are {nickels} nickel(s),")
-print(f"and there are {pennies} penny(s).")
+print(f"there are {quarters} quarter(s), {dimes} dime(s), {nickels} nickel(s), and {pennies} penny(s).")
