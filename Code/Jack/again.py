90bf795a0855b32aaf0ae06cae99444e825b0c6a
Jack Bragg <Jack.d.bragg@gmail.com>
2019-06-07 17:59:55 -0700

    added all versions to lab 9 and fixed falsey on again.py

--- Code/Jack/again.py
+++ Code/Jack/again.py
@@ -1,9 +1,12 @@
 def again(question):
-    choice = input(f'{question} y/n\n')
-    if choice == 'n':
-        return True
-    elif choice == 'y':
-        return False
-    else:
-        print('Incorrect input')
-        return False
+    TF = True
+    while TF:
+        choice = input(f'{question} y/n\n')
+        if choice == 'n':
+            TF = False
+            return True
+        elif choice == 'y':
+            TF = False
+            return False
+        else:
+            print('Incorrect input')
--- Code/Jack/lab_09_Unit_Converter.py
+++ Code/Jack/lab_09_Unit_Converter.py
@@ -1,41 +1,52 @@
 import math
-import decimal
 import again
 
 
-decimal.getcontext().prec = 5
-
+# Defines user choices
+s_Units_Allowed = ['ft', 'mi', 'm', 'km', 'yd', 'inch']
 
 class Converter(object):
-    """docstring for Converter."""
-    ft = {'ft':1, 'mi':1/5280, 'm':1/3.28084, 'km':1/3280.84}
-    mi = {'ft':5280, 'mi':1, 'm':1609.34, 'km':1.60934}
-    m = {'ft':3.28084, 'mi':1/1609.34, 'm':1, 'km':1/1000}
-    km = {'ft':3280.84, 'mi':1/1.60934, 'm':1000, 'km':1}
-    conv = {'ft':ft, 'mi':mi, 'm':m, 'km':km}
 
-    def __init__(self, f_Dist_In, s_Unit_In, s_Unit_Out):
+    ''' To add more units, create a new line with as such
+
+        U = U per m
+
+        and then append to the end of m = {...'U':U}
+
+        don't forget to add the choice to s_Units_Allowed
+    '''
+
+    ft = 0.3048
+    mi = 1609.34
+    km = 1000
+    yd = 0.9144
+    inch = 0.0254
+    m = {'ft':ft, 'mi':mi, 'm':1, 'km':km, 'yd':yd, 'inch':inch}
+
+
+    def __init__(self, dc_Dist_In, s_Unit_In, s_Unit_Out):
         super(Converter, self).__init__()
-        self.dist = f_Dist_In
         self.UI = s_Unit_In
         self.UO = s_Unit_Out
 
+        # Converts dist to meters
+        self.dist = dc_Dist_In
+
     def getConv(self):
-        #Finds corresponding values within nested dicts
-        uo = decimal.Decimal(self.conv[self.UI][self.UO])
-        new_Dist = decimal.Decimal(self.dist * uo)
-        return new_Dist
+
+        return (self.dist * self.m[self.UI]) * (1/self.m[self.UO])
 
 
 
-s_Units_Allowed = 'ft, mi, m, or km '
+
 while True:
-    f_Usr_Dist_In = decimal.Decimal(input('What is the distance you wish to convert? '))
-    s_Usr_Unit_In = input('What are the units measured? ' + s_Units_Allowed)
-    s_Usr_Unit_Out = input('What are the desired units? (ft, mi, m, or km) ')
-    usr_Conv = Converter(f_Usr_Dist_In, s_Usr_Unit_In, s_Usr_Unit_Out)
-    dc_Usr_Conv_Dist_Out = usr_Conv.getConv()
-    print(f'\nYour converted distance is : {dc_Usr_Conv_Dist_Out}{usr_Conv.UO}')
+    sep = ', '
+    units = sep.join(s_Units_Allowed)
+    dc_Usr_Dist_In = float(input('What is the distance you wish to convert? '))
+    s_Usr_Unit_In = input(f'What are the units measured? {units}: ')
+    s_Usr_Unit_Out = input(f'What are the desired units? {units}: ')
+    usr_Conv = Converter(dc_Usr_Dist_In, s_Usr_Unit_In, s_Usr_Unit_Out)
+    print(f'\nYour converted distance is : {usr_Conv.getConv():.2f}{usr_Conv.UO}')
     if again.again('\nWould you like to convert another distance?'):
         break
 
