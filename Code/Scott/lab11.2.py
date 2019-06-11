c646891f72c2bb2402b34054df8e986cceb2282a
tran0370 <tran0370@gmail.com>
2019-06-11 16:18:07 -0700

    Fixed 3 versions of Lab 11

--- Code/Scott/lab11.2.py
+++ Code/Scott/lab11.2.py
@@ -3,7 +3,7 @@ Name:           Scott Tran
 Date            6/7/2019
 Assignment:     Write a simple REPL (read evaluate print loop) calculator that can handle addition, subtraction, multiplication, and division. Ask the user for an operator and each operand. Allow the user to keep performing operations until they say 'done'
 """
-
+import operator
 
 def operation():
     while True:
@@ -11,6 +11,7 @@ def operation():
         if x in ("+", "-", "*", "/"):
             return x
         elif x == "done":
+            print("Thank you, come again.")
             quit()
         else:
             print(f"{x} was not one of the selections. Please try again...") 
@@ -20,7 +21,7 @@ def first_num():
     while True:
         x = input("What is the first number? ")
         if x.isdigit():
-            return x
+            return int(x)
         elif x == "done":
             quit()
         else:
@@ -31,16 +32,28 @@ def second_num():
     while True:
         x = input("What is the second number? ")
         if x.isdigit():
-            return x
+            return int(x)
         elif x == "done":
             quit()
         else:
             print(f"{x} was not an integer number. Please try again...") 
 
+def arithmetic(a, b):
+    if x == "+":
+        return operator.add(a, b)
+    elif x == "-":
+        return operator.sub(a, b)
+    elif x == "*":
+        return operator.mul(a, b)
+    elif x == "/":
+        return operator.truediv(a, b)
+    else:
+        print("error!!!!")
+
 while True:
     x = operation()
     y = first_num()
     z = second_num()
-    a = int(y) + int(z)
 
-    print(f"{y} {x} {z} = {a}")
+    print(f"{y} {x} {z} = {arithmetic(y, z)}")
+
