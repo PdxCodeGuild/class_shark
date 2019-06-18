f60b808be604307e5cf89e23b56b81f9e1bb4673
tomaccosheep <al.burns.email@gmail.com>
2019-06-17 18:09:58 -0700

    Update my_pillow.py
    Updated numbers

--- Code/Al/my_pillow.py
+++ Code/Al/my_pillow.py
@@ -3,7 +3,7 @@ from PIL import Image
 from random import randrange
 img = Image.new('RGB', (4, 4))
 pixels = img.load()
-for i in range(img.size[0]): # 50
-    for j in range(img.size[1]):
-        pixels[i, j] = (0, int(255*(i/4)), int(255*(j/4)))
+for i in range(img.size[0]): # 4
+    for j in range(img.size[1]): # 4
+        pixels[i, j] = (0, int(255*(i/3)), int(255*(j/3)))
 img.show()
