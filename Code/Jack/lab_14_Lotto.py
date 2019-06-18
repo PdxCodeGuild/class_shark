3cc286e781852a0c82d55c981b9d838179a8219f
Jack Bragg <Jack.d.bragg@gmail.com>
2019-06-17 19:39:36 -0700

    updated lab 14

--- Code/Jack/lab_14_Lotto.py
+++ Code/Jack/lab_14_Lotto.py
@@ -13,47 +13,32 @@ def make_Ticket():
     return [randint(1, 99) for _ in range(6)]
 
 
-def winnings(num_matched):
-    win = 0
-    if num_matched == 6:
-        print('YOU WON $25M!!!')
-        win = 25000000
-    if num_matched == 5:
-        print('You won $1M!')
-        win = 1000000
-    if num_matched == 4:
-        win = 50000
-    if num_matched == 3:
-        win = 100
-    if num_matched == 2:
-        win = 7
-    if num_matched == 1:
-        win = 4
-    return win
+def find_matches(win_ticket, my_ticket):
+    winnings = [0, 4, 7, 100, 50000, 1000000, 25000000]
+    match = 0
+    for i in range(6):
+        if my_ticket[i] == win_ticket[i]:
+            match += 1
+    return winnings[match]
 
 
 def main():
     money = 0
-    winner = make_Ticket()
     num_of_wins = 0
     earnings = 0
     expenses = float(num_of_tickets * 2)
+
+    winner = make_Ticket()
+
     for itm in range(num_of_tickets):
-        match = 0
-        my_ticket = make_Ticket()
         money -= 2
-        # i = 0
-        for i in range(len(winner)):
-            if my_ticket[i] == winner[i]:
-                match += 1
-                num_of_wins += 1
-        # for num in winner:
-        #     if num == my_ticket[i]:
-        #         match += 1
-        #         num_of_wins += 1
-        #     i += 1
-        earnings += winnings(match)
-        money += winnings(match)
+        my_ticket = make_Ticket()
+        t_money = find_matches(winner, my_ticket)
+        if t_money > 0:
+            money += t_money
+            earnings += t_money
+            num_of_wins += 1
+
     print('\nwinning numbers:', winner
           , '\nMoney: $', money
           , '\nYou won', num_of_wins, 'time(s)'
