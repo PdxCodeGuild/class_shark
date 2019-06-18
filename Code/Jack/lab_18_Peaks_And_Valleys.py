d4ad7ae1816627877487baf818f73d8c442a7837
Jack Bragg <Jack.d.bragg@gmail.com>
2019-06-17 21:29:24 -0700

    updated lab 18

--- Code/Jack/lab_18_Peaks_And_Valleys.py
+++ Code/Jack/lab_18_Peaks_And_Valleys.py
@@ -20,18 +20,19 @@ def peaks_and_valleys(l_data):
 
 def x_draw(l_data):
     print_out = ''
+    j = find_peaks(l_data)
     rows = max(l_data) + 1
-    print(rows)
+    print(rows, '\n', '*'*50)
+    prev_num = j[0]
     for i in range(rows, 0, -1):
         for num in l_data:
-            if num >= i:
+            if (num < prev_num) and (num < i):
+                print_out += ' O '
+            elif num >= i:
                 print_out += ' X '
             else:
-                for j in find_peaks(l_data):
-                    if i > j:
-                        print_out += ' O '
-                    else:
-                        print_out += '   '
+                print_out += '   '
+            prev_num = num
         print_out += '\n'
     return print_out
 
