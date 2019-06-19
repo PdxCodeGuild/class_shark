ec19d8d8f68522034eb5be8cd44042225807dcb6
Jack Bragg <Jack.d.bragg@gmail.com>
2019-06-18 20:36:31 -0700

    added new folder

--- Code/Jack/lab_13_ROT_Cipher.py
+++ Code/Jack/lab_13_ROT_Cipher.py
@@ -1,37 +1,51 @@
 import string
 
 
-def rot_Cipher(rot_i, usr_In):
+def encode(rot_i, usr_In):
     l_in = list(usr_In.strip())
     print(l_in)
-
-    # ciph = [(string.ascii_letters[string.ascii_letters.find(letter)
-    #         + rot_i - 52]) for letter in l_in]
-
     ciph = []
+    lower = string.ascii_lowercase
+    upper = string.ascii_uppercase
+
     for letter in l_in:
         if letter.islower():
-            ciph.append(string.ascii_lowercase[
-                        string.ascii_lowercase.find(letter)
-                        + rot_i - 26])
+            ciph.append(lower[
+                        (lower.find(letter) + rot_i) % 26])
         elif letter.isupper():
-            ciph.append(string.ascii_uppercase[
-                        string.ascii_uppercase.find(letter)
-                        + rot_i - 26])
+            ciph.append(upper[
+                        (upper.find(letter) + rot_i) % 26])
         else:
-            ciph.append(' ')
+            ciph.append(letter)
 
     return ''.join(ciph)
 
+
+def decode(rot_i, usr_In):
+    return encode(-rot_i, usr_In)
+
+
+def dict_encode(rot_i, usr_In):
+    translate = dict(zip(string.ascii_letters
+                         , string.ascii_letters[rot_i:]
+                         + string.ascii_letters[:rot_i]))
+    cyph = ''
+    for letter in usr_In:
+        cyph += translate.get(letter, letter)
+    return cyph
+    # return ''.join([translate.get(letter, letter) for letter in usr_In])
 
 TF = True
 while TF:
     i_usr_rot = int(input('how much rot: '))
     s_usr_str = input('please enter a sentence to encode: ')
-    cip = rot_Cipher(i_usr_rot, s_usr_str)
-    cip = cip.replace('0', ' ')
-    print("Your string was:\n", s_usr_str, '\nYour encoded message is:\n'
+    cip = dict_encode(i_usr_rot, s_usr_str)
+
+    print("Your string was:\n"
+          , s_usr_str
+          , '\nYour encoded message is:\n'
           , cip)
+
     choice = input('again? y/n ')
     if choice == 'n':
         TF = False
