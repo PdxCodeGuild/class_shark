67ba33ad0712719107bc2ba50dfb38837d05ae1d
Jack Bragg <Jack.d.bragg@gmail.com>
2019-06-18 19:43:37 -0700

    update to lab 20

--- Code/Jack/lab_20_Credit_Card_verf.py
+++ Code/Jack/lab_20_Credit_Card_verf.py
@@ -3,6 +3,7 @@ class CreditCardValid(object):
 
     li_ccn = []
     i_chk_d = 0
+    valid = False
 
     def __init__(self, s_ccn):
         super(CreditCardValid, self).__init__()
@@ -10,7 +11,24 @@ class CreditCardValid(object):
         self.str_to_int()
         self.li_ccn.reverse()
         self.i_chk_d = self.li_ccn.pop(0)
+        self.double_ev_other()
+        self.sub_nine()
+        if self.i_chk_d == sum(self.li_ccn):
+            self.valid = True
 
     def str_to_int(self):
         for i in range(len(self.li_ccn)):
-            self.li_ccn[i] = int(self.li_ccn[i])    
+            self.li_ccn[i] = int(self.li_ccn[i])
+
+    def double_ev_other(self):
+        for i in range(len(self.li_ccn)):
+            if i % 2 == 0:
+                self.li_ccn[i] *= 2
+
+    def sub_nine(self):
+        for i in range(len(self.li_ccn)):
+            if self.li_ccn[i] > 9:
+                self.li_ccn -= 9
+
+    def get_valid(self):
+        return self.valid
