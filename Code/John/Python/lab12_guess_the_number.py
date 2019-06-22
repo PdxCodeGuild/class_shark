151e5a11ccd901ae5094370b8dd66129e4d57af7
John Fittro <J.Fittro@gmail.com>
2019-06-21 20:59:53 -0700

    Created Python Folder and Lab0

--- Code/John/Python/lab12_guess_the_number.py
+++ Code/John/Python/lab12_guess_the_number.py
@@ -4,7 +4,8 @@ import random
 num1 = random.randint(1, 10)
 tries = 0
 while tries < 10:
-    guess = int(input('\n' + 'Guess a number between 1 and 10, you have 10 tries! > '))
+    guess = int(input('\n' + 'Guess a number between 1 and 10, you have 10\
+                tries! > '))
 #    guess = int(guess)
     if guess == num1:
         print('\n' + 'You guessed it!')
@@ -12,4 +13,4 @@ while tries < 10:
     else:
         print('\n' + f'Sorry, incorrect!')
         tries += 1
-print('\n' + f'You used a total of  {tries} tries')
+print('\n' + f'You used a total of {tries} tries')
