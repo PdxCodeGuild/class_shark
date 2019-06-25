97721eaaf78fc2d20bcdafd335684c824ef09213
Scott Tran <Tran0370@gmail.com>
2019-06-25 15:22:11 -0700

    added Lab 16

--- Code/Scott/practice.py
+++ Code/Scott/practice.py
@@ -1,7 +1,18 @@
-words = ['a', 'b', 'a', 'c', 'd']
-stopwords = ['a', 'c']
-for word in list(words):  # iterating on a copy since removing will mess things up
-    if word in stopwords:
-        words.remove(word)
+# words = ['a', 'b', 'a', 'c', 'd']
+# words2 = list(words)
+# stopwords = ['a', 'c']
+# for word in words2:  # iterating on a copy since removing will mess things up
+#     if word in stopwords:
+#         words.remove(word)
 
-print(words)
+# print(words)
+
+nums = [[5,2,3],[4,5,1],[7,6,3]]
+# new = []
+
+# for num in nums:
+#     new.extend(num)
+# print(new)
+
+d = [new.extend(num) for num in nums]
+print(d)
