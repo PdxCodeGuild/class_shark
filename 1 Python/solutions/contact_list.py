b97b8b4ba22019f98be8177ccbdb105278a37ac3
DED8IRD <ewu72829@gmail.com>
2019-06-27 20:10:33 -0700

    contact_list partial

--- 1 Python/solutions/contact_list.py
+++ 1 Python/solutions/contact_list.py
@@ -14,17 +14,35 @@ def read_csv(filename):
 
     parses csv and returns dictionary of contacts
     '''
-    pass
+    with open(filename) as f:
+        contents = f.read()
+
+    lines = contents.split('\n')
+    headers = lines[0].split(',')
+    contacts = {}
+    for i in range(1, len(lines)):
+        row = lines[i].split(',')
+        contact = dict(zip(headers, row))
+        contacts[contact['name']] = contact
+    return contacts
 
 
-def write_csv(contacts, filename):
+def write_csv(contacts, headings, filename):
     '''
     :contacts: dict of contact list
     :filename: str of csv filepath
 
     converts contact list to csv and writes to file
     '''
-    pass
+    headings = ','.join(headings)
+    lines = [headings]
+    for name in contacts:
+        row = ','.join(contacts[name].values())
+        lines.append(row)
+    lines = '\n'.join(lines)
+
+    with open(filename, 'w') as f:
+        f.write(lines)
 
 
 def create(contacts, contact):
@@ -34,7 +52,8 @@ def create(contacts, contact):
 
     creates contact and adds to contact list
     '''
-    pass
+    contacts[contact['name']] = contact
+    return f"Added {contact['name']}"
 
 
 def read(contacts, name):
@@ -44,7 +63,7 @@ def read(contacts, name):
 
     returns contact if name found in contact list
     '''
-    pass
+    return contacts.get(name, f'{name} not found')
 
 
 def update(contacts, name, details):
@@ -55,7 +74,9 @@ def update(contacts, name, details):
 
     updates contact if name found in contact list
     '''
-    pass
+    if name in contacts:
+        contacts[name].update(details)
+    return read(contacts, name)
 
 
 def delete(contacts, name):
@@ -65,14 +86,43 @@ def delete(contacts, name):
 
     deletes contact if name found in contact list
     '''
-    pass    
+    if name in contacts:
+        del contacts[name]
+        return f'Deleted {name}'
+    else:
+        return f'{name} not found'
+
+
+def print_contact(contacts, name):
+    for key, val in contacts[name].items():
+        print(f'{key}: {val}')
+
+
+def print_all(contacts):
+    print('-'*60)
+    for contact in contacts:
+        print_contact(contacts, contact)
+        print('-'*60)
 
 
 def repl():
     '''
     REPL user interface
     '''
-    pass
+    contact_list = read_csv('contact_list.csv')
+    print_all(contact_list)
+    print(read(contact_list, 'bobra'))
+    print(read(contact_list, 'bobrah'))
+    print(delete(contact_list, 'bobra'))
+    print_all(contact_list)
+    print(delete(contact_list, 'bobrah'))
+    print_all(contact_list)
+    print(create(contact_list, {'name': 'rabob', 'age':'600000', 'job': 'embrolmber'}))
+    print_all(contact_list)
+    print(update(contact_list, 'rabob', {'job': 'captain embrholmhber'}))
+    print_all(contact_list)
+    print(update(contact_list, 'rabobra', {'job': 'captain embrholmhber'}))
+    write_csv(contact_list, ['name', 'age', 'job'], 'contact_list2.csv')
 
 
 if __name__ == '__main__':
