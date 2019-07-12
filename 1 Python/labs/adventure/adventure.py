757b62daf8833354b3bc15281ae8a0cc0532fd47
DED8IRD <ewu72829@gmail.com>
2019-07-11 19:11:56 -0700

    group project + starter code

---
+++ 1 Python/labs/adventure/adventure.py
@@ -0,0 +1,8 @@
+# adventure.py
+'''
+Command line adventure game
+'''
+from entities import *
+from world import *
+
+# Game logic goes here
---
+++ 1 Python/labs/adventure/world.py
@@ -0,0 +1,23 @@
+class World:
+    '''
+    Represents map in adventure game
+    '''
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.map = 'your map representation goes here'
+
+    def __str__(self):
+        '''
+        Map string representation
+        '''
+        pass 
+
+    def move(self, entity, x, y):
+        '''
+        Move entity on map
+        '''
+        pass
+
+    # any other methods
+
