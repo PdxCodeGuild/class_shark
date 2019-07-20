17360231f9c8ff4098296c7c2c879a7f211f5cce
DED8IRD <ewu72829@gmail.com>
2019-07-19 21:11:35 -0700

    landing page example (wip)

---
+++ 2 HTML + CSS/solutions/phosho/script.js
@@ -0,0 +1,7 @@
+// script.js
+
+// navbar collapse logic
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, {});
+});
