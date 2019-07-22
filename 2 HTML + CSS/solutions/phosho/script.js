8753377512bffc9f2165b417eeaa47d6792c3613
DED8IRD <ewu72829@gmail.com>
2019-07-22 13:45:07 -0700

    non-jquery dependent parallax landing page

--- 2 HTML + CSS/solutions/phosho/script.js
+++ 2 HTML + CSS/solutions/phosho/script.js
@@ -5,3 +5,9 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, {});
 });
+
+// parallax logic
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.parallax');
+  var instances = M.Parallax.init(elems, {});
+});
