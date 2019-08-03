e3fdba1c07ddb2757e5656267d1fc7d6cfc82c5c
DED8IRD <ewu72829@gmail.com>
2019-08-02 18:48:09 -0700

    splice example

--- 3 JavaScript/solutions/practice.js
+++ 3 JavaScript/solutions/practice.js
@@ -85,3 +85,15 @@ function bouncer(arr) {
 }
 
 
+/* Basic Algorithm Scripting: Slice and Splice
+You are given two arrays and an index.
+Use the array methods slice() and splice() to copy each element of the first array into the second array, in order.
+Begin inserting elements at index n of the second array.
+Return the resulting array. The input arrays should remain the same after the function runs. */
+
+function frankenSplice(arr1, arr2, n) {
+  let ret = arr2.slice()      // copy of arr2
+  ret.splice(n, 0, ...arr1);  // at index n, remove 0 elements,
+                              // and insert all the elements of arr1
+  return ret
+}
