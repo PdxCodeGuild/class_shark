aed25357c1e8e2e34a1764d8de282add0a64aaab
jackrocker418 <jackrocker418@gmail.com>
2019-08-03 00:25:59 -0700

    update to rot 13

--- Code/Jack/JavaScript/lab_01-03_pick_3/lab_02_rot13/js/rot13.js
+++ Code/Jack/JavaScript/lab_01-03_pick_3/lab_02_rot13/js/rot13.js
@@ -24,5 +24,37 @@ function theFunc(ele) {
 	document.querySelector(id='#btnOne').style.backgroundColor = color;
 }
 
+function encode(msg) {
+	let encMsg = ''
+	for (let i = 0; i < msg.length; i++) {
+		if (isUpperCase(msg[i])) {
+			encMsg += (ciphUp.indexOf(msg[i]) + rotNum) % 26);
+		} else if (isLowerCase(msg[i])) {
+			encMsg += (ciphLo.indexOf(msg[i]) + rotNum) % 26);
+		} else {	encMsg += msg[i]	}
+	}
+	return encMsg
+}
+////////////////////////////////////////////////
+
+const rotNum = querySelector('#rot-val').value
+rotNum = parseInt(rotNum)
+const ciphLo = 'abcdefghijklmnopqrstuvwxyz'
+const ciphUp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+
+function timeToEncode(ele) {
+document.querySelector('#cipher-form').innerHTML = "Your message: <input type='text' id ='msg' placeholder='message'><br><div class='container'><div class='my-btn-dec'><button id='btnTwo'>Encode</button>"
+let msg = document.querySelector('#msg').value
+const msgBtn = document.querySelector('#btnTwo')
+msgBtn.addEventListener('click', (evt) => {
+	msg = encode(msg);
+document.querySelector('#encMsg').innerHTML = "Your message: " + `${msg}`
+})
+}
+
+
+///////////////////////////////////////////////
+
 const btn = document.querySelector('#btnOne')
-btn.addEventListener('click', theFunc(this))
+btn.addEventListener('click', timeToEncode(this))
