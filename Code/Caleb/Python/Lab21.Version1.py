3b436cfe614d4ca90f8587903ec60c0afa3c638c
Caleb Mealey <dcmealey@gmail.com>
2019-08-06 00:48:35 -0700

    Updated file, adding encoding to fix error

--- Code/Caleb/Python/Lab21.Version1.py
+++ Code/Caleb/Python/Lab21.Version1.py
@@ -25,9 +25,11 @@ You'll find that the most common words aren't particularly interesting (lots of 
 from string import punctuation
 STOPWORDS = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 
+print('Welcome to Version 1 of Lab 21 - Count Words. A program written in python by Caleb Mealey')
 
 def openfile(file_location):
-    with open(f'{file_location}', 'r') as b:
+    print(f'The 10 most reoccuring words found in {file_location} outside of the standard common words listed in the "stop words" list are:')
+    with open(f'{file_location}', 'r', encoding="utf8") as b:
         book_text = b.read()
         book_text = book_text.lower()
         translator = str.maketrans('', '', punctuation)
@@ -42,8 +44,11 @@ def openfile(file_location):
         words.sort(key=lambda tup: tup[1], reverse=True)  # sort largest to smallest, based on count
         for i in range(min(10, len(words))):  # print the top 10 words, or all of them, whichever is smaller
             print(words[i])
+    print("---"*50)
 
 
 if __name__ == "__main__":
-    # openfile('./assets/AdventuresofHuckleberryFinn.txt')
+    openfile('./assets/AdventuresofHuckleberryFinn.txt')
     openfile('./assets/TheKingJamesBible.txt')
+    openfile('./assets/MobyDick.txt')
+    openfile('./assets/TheAdventuresofSherlockHolmes.txt')
