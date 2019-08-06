df267f2c44f9ca455b34beeec94e781bc101d623
DED8IRD <ewu72829@gmail.com>
2019-08-05 19:18:59 -0700

    chunky monkey

--- 3 JavaScript/solutions/practice.js
+++ 3 JavaScript/solutions/practice.js
@@ -97,3 +97,18 @@ function frankenSplice(arr1, arr2, n) {
                               // and insert all the elements of arr1
   return ret
 }
+
+
+/* Basic Algorithm Scripting: Chunky Monkey
+Write a function that splits an array (first argument) into 
+groups the length of size (second argument) 
+and returns them as a two-dimensional array.
+*/
+function chunkArrayInGroups(arr, size) {
+  let chunks = []
+  for (let i=0; i<arr.length; i+=size) {
+    chunks.push(arr.slice(i, i+size))
+  }
+  return chunks  
+}
+
