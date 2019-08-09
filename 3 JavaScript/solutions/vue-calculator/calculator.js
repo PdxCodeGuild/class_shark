6b6e5b1e492e7b42d706ccfea09fb4918cae2543
DED8IRD <ewu72829@gmail.com>
2019-08-08 21:53:52 -0700

    vue calculator implementation

--- 3 JavaScript/solutions/vue-calculator/calculator.js
+++ 3 JavaScript/solutions/vue-calculator/calculator.js
@@ -1,192 +1,162 @@
 // calculator.js
 
-// selectors
-const display = document.querySelector('#display')
-const history = document.querySelector('#history')
-const digits = document.querySelectorAll('.digit')
-const operators = document.querySelectorAll('.op')
-const decimal = document.querySelector('#dec')
-const equal = document.querySelector('#eq')
-const ce = document.querySelector('#ce')
-const ac = document.querySelector('#ac')
+// vue app
+const app = new Vue({
+  el: '#calc-body',
+  data: {
+    display: null,
+    history: [],
+    ans: null,
+    op: null,
+    currentNumber: '',
+    isDecimal: false
+  },
+  methods: {
+    calculate: function() {
+      // ans op currentNumber, if the currentNumber exists
+      if (this.currentNumber) {
+        this.currentNumber = parseFloat(this.currentNumber)
+        this.history.push(this.currentNumber)
+        this.ans = (this.ans !== null ? this.ans : 0)
+        if (this.op === '+') {
+          this.ans += this.currentNumber
+        } else if (this.op === '-') {
+          this.ans -= this.currentNumber
+        } else if (this.op === '*' || this.op === '×') {
+          this.ans *= this.currentNumber
+        } else if (this.op === '/' || this.op === '÷') {
+          this.ans /= this.currentNumber
+        }
+      }
+      this.clearEntry()
+    },
+    clearEntry: function() {
+      this.currentNumber = ''
+      this.isDecimal = false
+      this.display = this.ans      
+    },
+    allClear: function() {
+      this.ans = null
+      this.history = []
+      this.op = null
+      this.clearEntry()
+    },
+    addDigit: function(digit) {
+      this.currentNumber += digit
+      this.display = this.currentNumber
+    },
+    addDecimal: function() {
+      if (!this.isDecimal) {
+        this.currentNumber += '.'
+        this.isDecimal = true
+        this.display = this.currentNumber
+      }  
+    },
+    addOperator: function(operator) {
+      if (!this.currentNumber && 
+          isNaN(this.history[this.history.length-1]) && 
+          operator === '-') {
+            this.currentNumber = '-'
+      } else {
+        // if an operator is already set, calculate ans op currentNumber
+        if (this.op) {
+          this.calculate()
+        // else if no ans is set, set ans as currentNumber or 0 if currentNumber is null
+        } else if (!this.ans && this.ans !== 0) {
+          this.ans = (this.currentNumber ? parseFloat(this.currentNumber) : 0)
+        }
+        this.op = operator
+        // add currentNumber and operator to history
+        if (this.currentNumber) this.history.push(parseFloat(this.currentNumber))
+        this.history.push(this.op)
+        // display the operator
+        this.clearEntry()
+        this.display = operator    
+        }  
+    },
+    handleDigitClick: function(event) {
+      const digit = event.target.innerText
+      this.addDigit(digit)
+    },
+    handleOperatorClick: function(event) {
+      const op = event.target.innerText
+      this.addOperator(op)
+    },
+  },
+  computed: {
+    formattedHistory: function() {
+      const history = this.history.join('')
+      return history.length > 17 ? '...'+history.slice(-20) : history
+    },
+    formattedDisplay: function() {
+      // format number so it doesn't overflow the display
+      if (this.display === '' || this.display === null) return ''
+      // display operators as is
+      if (isNaN(this.display)) return this.display
 
-// variables
-let operation = []      // history of operations
-let ans = null          // current answer
-let op = null           // current operator
-let currentNumber = ''  // current number
-let isDecimal = false   // decimal flag
-const symbols = {
-  '+': 'add',
-  '-': 'sub',
-  '×': 'mult',
-  '*': 'mult',
-  '÷': 'div',
-  '/': 'div',   
-  '.': 'dec',
-  '=': 'eq',
-  'Enter': 'eq',
-  'CE': 'ce',
-  'AC': 'ac'
-}
+      let value = parseFloat(this.display)
+      // if value is a large int, set precision
+      if (value > 99999999) { 
+        return value.toPrecision(8)      
+      }
 
-// functions
+      // if value is a float, set precision and trim trailing 0s
+      if (value % 1 ) {
+        return parseFloat(value.toPrecision(8))
+      }
 
-// computes a op b
-function calculate(a, op, b) {
-  if (op === '+') {
-    return a + b
-  } else if (op === '-') {
-    return a - b
-  } else if (op === '*' || op === '×') {
-    return a * b
-  } else if (op === '/' || op === '÷') {
-    return a / b
-  }
-}
-
-// resets currentNumber and isDecimal and displays value and history onscreen
-function clearAndDisplay(value) {
-  if (value === null) value = 0 
-  currentNumber = ''
-  isDecimal = false  
-
-  // format number so it doesn't overflow the display
-  // if value is a float, set precision and trim trailing 0s
-  if (value % 1 ) {
-    value = parseFloat(value.toPrecision(8))
-  }
-  // if value is a large int, set precision
-  if (value > 99999999) { 
-    value = value.toPrecision(8)      
-  }
-  // display value and history onscreen
-  display.innerText = value
-  history.innerText = operation.join('')
-}
-
-// adds digit to currentNumber and displays it onscreen
-function addDigit(digit) {
-    currentNumber += digit
-    display.innerText = currentNumber  
-}
-
-// adds decimal to currentNumber
-function addDecimal() {
-  if (!isDecimal) {
-    currentNumber += '.'
-    display.innerText = currentNumber
-    isDecimal = true
-  }  
-}
-
-// adds operator
-function addOperator(operator) {
-  // if an operator is already set, calculate ans op currentNumber
-  if (op && currentNumber) {
-    ans = calculate(ans, op, parseFloat(currentNumber))
-  // else if no ans is set, set ans as currentNumber or 0 if currentNumber is null
-  } else if (!ans && ans !== 0) {
-    ans = (currentNumber ? parseFloat(currentNumber) : 0)
-  }
-  op = operator
-  // add currentNumber and operator to history
-  if (currentNumber) operation.push(parseFloat(currentNumber))
-  operation.push(op)
-  // display the operator
-  clearAndDisplay(op)
-}
-
-// compute ans op currentNumber and display onscreen
-function equals() {
-  if (currentNumber) {
-    operation.push(parseFloat(currentNumber))
-    ans = (ans !== null ? ans : 0)
-    ans = calculate(ans, op, parseFloat(currentNumber))
-  }
-  clearAndDisplay(ans)  
-}
-
-// reset all values
-function clearAll() {
-  operation = []
-  ans = null
-  op = null
-  clearAndDisplay(ans)  
-}
-
-// event listeners
-
-// button event listeners
-
-// digits.forEach((digit) => {
-//   digit.addEventListener('click', (evt) => {
-//     display.innerText = digit.innerText
-//   })
-// })
-
-// same logic for each digit button
-for (let digit of digits) {
-  digit.addEventListener('click', (evt) => {
-    addDigit(digit.innerText)
-  })  
-}
-
-decimal.addEventListener('click', (evt) => {
-  addDecimal()
-})  
-
-// same logic for each operator button
-for (let operator of operators) {
-  operator.addEventListener('click', (evt) => {
-    addOperator(operator.innerText)
-  })
-}
-
-equal.addEventListener('click', (evt) => {
-  equals()
-})
-
-ce.addEventListener('click', (evt) => {
-  clearAndDisplay(ans)
-})
-
-ac.addEventListener('click', (evt) => {
-  clearAll()
-})
-
-// keypress event listener to enable keyboard input
-window.addEventListener('keydown', evt => {
-  console.log(evt)
-  // set logic for specific keys
-  if (!isNaN(evt.key)) { // digits 
-    // style button with corresponding key
-    let button = document.querySelector(`#btn-${evt.key}`)
-    button.classList.add('active')
-    addDigit(evt.key)
-  } else {
-    // style button with corresponding key
-    let button = document.querySelector(`#${symbols[evt.key]}`)
-    if (button) button.classList.add('active')
-    if ('+-*/'.includes(evt.key)) { // operators
-      addOperator(evt.key)
-    } else if (evt.key === '.') { // decimal
-      addDecimal()
-    } else if (['=', 'Enter'].includes(evt.key)) { // = or enter
-      equals()
-    } else if (evt.key === 'Backspace') { // backspace
-      clearAndDisplay(ans)
+      // return number if it needs no formatting
+      return value
     }
-  } 
-})
+  },
+  mounted: function() {
+      const symbols = {
+        '+': 'add',
+        '-': 'sub',
+        '×': 'mult',
+        '*': 'mult',
+        '÷': 'div',
+        '/': 'div',   
+        '.': 'dec',
+        '=': 'eq',
+        'Enter': 'eq',
+        'CE': 'ce',
+        'AC': 'ac'
+      }  
 
-window.addEventListener('keyup', evt => {
-  // handle digits separately, since they have different selectors
-  if (!isNaN(evt.key)) { // digits 
-    let button = document.querySelector(`#btn-${evt.key}`)
-    if (button) button.classList.remove('active')
-  } else {
-    let button = document.querySelector(`#${symbols[evt.key]}`)
-    if (button) button.classList.remove('active')
+    // keypress event listener to enable keyboard input
+    window.addEventListener('keydown', evt => {
+      // set logic for specific keys
+      if (!isNaN(evt.key)) { // digits 
+        // style button with corresponding key
+        let button = document.querySelector(`#btn-${evt.key}`)
+        button.classList.add('active')
+        this.addDigit(evt.key)
+      } else {
+        // style button with corresponding key
+        let button = document.querySelector(`#${symbols[evt.key]}`)
+        if (button) button.classList.add('active')
+        if ('+-*/'.includes(evt.key)) { // operators
+          this.addOperator(evt.key)
+        } else if (evt.key === '.') { // decimal
+          this.addDecimal()
+        } else if (['=', 'Enter'].includes(evt.key)) { // = or enter
+          this.calculate()
+        } else if (evt.key === 'Backspace') { // backspace
+          this.clearEntry()
+        }
+      } 
+    })
+
+    window.addEventListener('keyup', evt => {
+      // handle digits separately, since they have different selectors
+      if (!isNaN(evt.key)) { // digits 
+        let button = document.querySelector(`#btn-${evt.key}`)
+        if (button) button.classList.remove('active')
+      } else {
+        let button = document.querySelector(`#${symbols[evt.key]}`)
+        if (button) button.classList.remove('active')
+      }
+    })
   }
 })
