d61f5362ca235a780a26fd144abbafa319b48c13
Caleb Mealey <dcmealey@gmail.com>
2019-08-14 23:31:32 -0700

    Changed some print statements, made UX clean & fully finished product

--- Code/Caleb/Python/Lab25.py
+++ Code/Caleb/Python/Lab25.py
@@ -21,17 +21,6 @@ Allow the user to enter commands into a REPL.
 > check balance
 > balance: $5
 '''
-
-
-'''
-Lab 25: ATM
-Let's represent an ATM with a class containing a balance property. A newly created account will default to a balance of 0. Implement the initializer, as well as the following methods:
-
-check_balance() returns the account balance
-deposit(amount) deposits the given amount in the account
-check_withdrawal(amount) returns true if the withdrawn amount won't put the account in the negative
-withdraw(amount) withdraws the amount from the account and returns it
-'''
 commands = ['c', 'check', 'check balance','check your balance', 'd', 'deposit funds', 'deposit', 'w', 'withdraw', 'withdraw funds', 'l', 'list', 'list transactions', 'list the transactions', 'list all', 'list all transactions', 'e', 'x', 'exit']
 
 
@@ -48,7 +37,7 @@ class ATM():
 
     def __init__(self):
         '''
-        initializer for the class
+        initializer for the class, initialize the balance property to 0. Reset a blank transaction history list.
         '''
         self.balance = 0
         self.transaction_hist = []
@@ -57,6 +46,7 @@ class ATM():
 
     def withdraw_check(self, withdraw_amt):
         '''
+        Function that returns False if the withdraw amount is greater than the balance, or it returns True
         '''
         if withdraw_amt > self.balance:
             return False
@@ -65,40 +55,45 @@ class ATM():
     
     def withdraw(self):
         '''
+        Function that contains the logic to request the amount to be drawn, call the withdraw_check function, if it's true withdraw that amount from the acccount, or print that there is insufficient funds in the account for the withdrawal.
         '''
         withdraw_amt = int(input('Enter the amount you would like to withdraw from your account: '))
         if self.withdraw_check(withdraw_amt):
             self.balance -= withdraw_amt
             self.check_balance()
-            self.transaction_hist.append(f'Customer withdrew ${withdraw_amt}')
-            print(f'You successfully withdrew ${withdraw_amt}')
+            self.transaction_hist.append([f'Customer withdrew: ${withdraw_amt}', f'{self.check_balance()}'])
+            print(f'You successfully withdrew: ${withdraw_amt}')
         else:
-            print(f'You currently do not have enough funds to withdraw the amount of ${withdraw_amt}')
+            print(f'You currently have insufficient funds, you cannot withdraw: ${withdraw_amt}')
         pass
 
 
     def deposit(self):
         '''
+        Function that contains the logic to request the amount to be deposited, add that to the account balance, and list the deposit in the transaction history.
         '''
         dep_amt = int(input('Enter the amount you would like to deposit: $'))
         self.balance += dep_amt
-        self.transaction_hist.append(f'Customer deposited ${dep_amt}')
-        print(f'You have successfully deposited ${dep_amt} into your account.')
+        self.transaction_hist.append([f'Customer deposited: ${dep_amt}', f'{self.check_balance()}'])
+        print(f'You have successfully deposited: ${dep_amt}')
         pass
 
     
     def check_balance(self):
         '''
+        Function that returns the current balance in the account.
         '''
-        print(f'The current balance in the account is: ${self.balance}')
-        pass
+        return (f'Current balance: ${self.balance}')
         
     
     def list_transactions(self):
         '''
+        Function that prints the list of transaction history
         '''
-        print(self.transaction_hist)
+        for i in range(len(self.transaction_hist)):
+            print(self.transaction_hist[i])
         pass
+
 
     def main_menu(self):
         '''
@@ -113,7 +108,7 @@ class ATM():
             if user_command in commands:
                 if user_command == 'c' or user_command.startswith('c'):
                     active_session = False
-                    self.check_balance()
+                    print(self.check_balance())
                     self.main_menu()
                     pass
                 elif user_command == 'd' or user_command.startswith('d'):
