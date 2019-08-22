a72ea773dc6a1c933ec906a956c6a303b0c5b96b
DED8IRD <ewu72829@gmail.com>
2019-08-21 21:25:12 -0700

    added weather app

---
+++ 4 Django/solutions-shark/todos/todos/static/todos/js/weather.js
@@ -0,0 +1,86 @@
+// weather.js
+
+const app = new Vue({
+  el: '#weather-app',
+  delimiters: ['${', '}'],
+  data: {
+    location: null,
+    zip: null,
+    lat: null,
+    lon: null,
+    weather: null,
+    description: null,
+    icon: null,
+    hi: null,
+    lo: null,
+    temp: null,
+    catFact: null
+  }, 
+  methods: {
+    getWeatherData: async function() {
+      const endpoint = 'http://api.openweathermap.org/data/2.5/weather'
+      let config
+      if (this.zip) {
+        config = {
+          params: {
+            appid: '847b317dd0b65a4de6755ad19098a2c1',
+            zip: this.zip,
+            units: 'imperial'
+          }
+        }            
+      } else {
+        config = {
+          params: {
+            appid: '847b317dd0b65a4de6755ad19098a2c1',
+            lat: this.lat,
+            lon: this.lon,
+            units: 'imperial'
+          }
+        }  
+      }
+      const response = await axios.get(endpoint, config)
+      const data = response.data
+      const weather = data.weather[0]
+      console.log(response.data)
+      this.location = data.name
+      this.weather = weather.main
+      this.description = weather.description
+      this.icon = `http://openweathermap.org/img/wn/${weather.icon}.png`
+      this.hi = data.main.temp_max
+      this.lo = data.main.temp_min
+      this.temp = data.main.temp
+    },
+    getLocation: function() {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.lat = position.coords.latitude
+        this.lon = position.coords.longitude
+        localStorage.setItem('lat', this.lat)
+        localStorage.setItem('lon', this.lon)
+        this.getWeatherData()
+      })
+    },
+    getCatFact: async function() {
+      const endpoint = 'https://cors-anywhere.herokuapp.com/https://cat-fact.herokuapp.com/facts/random'
+      const config = {
+        params: {
+          animal_type: 'cat',
+          amount: 1
+        }
+      }
+      const response = await axios.get(endpoint, config)
+      this.catFact = response.data.text
+    }
+  },
+  mounted() {
+    const lat = localStorage.getItem('lat')
+    const lon = localStorage.getItem('lon')
+    if (lat && lon) {
+      this.lat = lat 
+      this.lon = lon
+      this.getWeatherData()
+    } else {
+      this.getLocation()
+    }
+    this.getCatFact()
+  }
+})
--- 4 Django/solutions-shark/todos/todos/urls.py
+++ 4 Django/solutions-shark/todos/todos/urls.py
@@ -5,5 +5,6 @@ from . import controller_views
 
 app_name = 'todos' # for namespacing
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='todos/template_view.html')), 
+    path('', TemplateView.as_view(template_name='todos/template_view.html'), name='index'), 
+    path('weather/', TemplateView.as_view(template_name='todos/weather_app.html'), name='weather'), 
 ]
