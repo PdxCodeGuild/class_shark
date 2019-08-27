d2d768dee51b8a3a3cca8591ce0a68128ae1b892
DED8IRD <ewu72829@gmail.com>
2019-08-26 20:35:13 -0700

    created RESTful API using django rest framework

--- 4 Django/solutions-shark/todos/todosite/urls.py
+++ 4 Django/solutions-shark/todos/todosite/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('todos.urls'))
+    path('', include('todos.urls')),
+    path('drf/', include('drf.urls'))
 ]
